8c231a077f8a3a1e5ef17f013a0f3c788581748c
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-10-28 16:29:52 -0600

    Lab

---
+++ laboratorio3/laboratorio3/DAO/CrdAsignatura.cs
@@ -0,0 +1,22 @@
+﻿using laboratorio3.Data;
+using laboratorio3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorio3.DAO
+{
+    public class CrdAsignatura
+    {
+        AsignaturaContext db = new AsignaturaContext();
+
+        public  CrdAsignatura Asignaturaindivi(int id)
+        {
+            var buscar = db.Asignatura.FirstOrDefault(x => x.id == id);
+            return buscar;
+        }
+
+    }
+}
---
+++ laboratorio3/laboratorio3/Data/AsignaturaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laboratorio3.Data
+{
+    public class AsignaturaContext : DbContext
+    {
+        public DbSet<AsignaturaContext> Asignatura { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=awita28;Database=lab3;Trusted_Connection=SSPI;MultipleActiveResultSets=true;TrustServerCertificate=true;");
+        }
+    }
+}
